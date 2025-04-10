e9279027a1839998920f453cde6d445a79040512
Boru Isako <isco.jaldy@gmail.com>
2025-04-10 20:28:42 +0300

    Complete labs

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,8 +6,22 @@ function Counter() {
   function increment() {
     setCount(count + 1);
   }
+  const [number,setIncrementByTwo] = useState(0)
+  function incrementByTwo(){
+    setIncrementByTwo(number + 2)
+  }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return (
+    <>  
+      <div>
+      <button onClick={increment}>I have been clicked {count} times</button>
+
+      </div>
+      <div style={{marginTop: "10px"}}>
+      Click to increment by 2: <button onClick={incrementByTwo}> {number}</button>
+        
+      </div>
+    </>)
 }
 
 export default Counter;
